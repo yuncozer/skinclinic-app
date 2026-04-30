921a5a0bbfeb561f83babff627b676fb32f9243c
danielcardenas <dcardenas@shokworks.io>
2026-04-30 11:02:56 -0400

    Habilitar login con autenticación de Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import DashboardClient from '@/components/DashboardClient';
 import { createClient } from '@/lib/superbase-server';
 
 export default async function Page() {
-  // const supabase = await createClient();
+  const supabase = await createClient();
 
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  // if (!user) {
-  //   redirect('/login');
-  // }
+  if (!user) {
+    redirect('/login');
+  }
 
   return <DashboardClient />;
 }
